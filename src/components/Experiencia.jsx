bb6fb463a4c980b86bfab62ec7c4d8df26f673b6
Your Name <you@example.com>
2025-11-20 17:27:57 -0500

    feat: Experiencia mapeada desde arreglo en App.jsx

--- src/components/Experiencia.jsx
+++ src/components/Experiencia.jsx
@@ -1,29 +1,24 @@
-    export default function Experiencia() {
-    const experiencia = [
-        { id: 1, puesto: "Desarrollador Full Stack", empresa: "Navisoft Gaming", periodo: "2022-2024" },
-        { id: 2, puesto: "Instructor TIC", empresa: "SENA", periodo: "2020-2022" }
-    ];
-
-    // Renderiza condicionalmente si no hay experiencia
-    if (experiencia.length === 0) {
-        return (
-        <section>
-            <h3>Experiencia Profesional</h3>
-            <p>No se ha registrado experiencia profesional.</p>
-        </section>
-        );
-    }
-
+export default function Experiencia({ listaExperiencia }) {
+  
+  if (listaExperiencia.length === 0) {
     return (
-        <section>
+      <section>
         <h3>Experiencia Profesional</h3>
-        <ul>
-            {experiencia.map((exp) => (
-            <li key={exp.id}>
-                <strong>{exp.puesto}</strong> - {exp.empresa} ({exp.periodo})
-            </li>
-            ))}
-        </ul>
-        </section>
+        <p>No se ha registrado experiencia profesional.</p>
+      </section>
     );
-    }
+  }
+
+  return (
+    <section>
+      <h3>Experiencia Profesional</h3>
+      <ul>
+        {listaExperiencia.map(({ id, puesto, empresa, periodo }) => (
+          <li key={id}>
+            <strong>{puesto}</strong> - {empresa} ({periodo})
+          </li>
+        ))}
+      </ul>
+    </section>
+  );
+}
